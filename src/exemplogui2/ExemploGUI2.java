13906e92386e668596102b337b1a52f4c1816dd4
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-05-16 09:08:29 +0200

    Commit git

---
+++ src/exemplogui2/ExemploGUI2.java
@@ -0,0 +1,18 @@
+
+package exemplogui2;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class ExemploGUI2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana obxVentana=new Ventana();
+        obxVentana.setVisible(true);
+    }
+    
+}
